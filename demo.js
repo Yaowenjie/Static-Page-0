78ca34a7a89cc763b121c3dd484f92e0f7050f73
Yaowenjie <wjyao@thoughtworks.com>
2016-05-30 16:31:08 +0800

    Add a survey link for clicking page

--- demo.js
+++ demo.js
@@ -2,7 +2,9 @@ window.onclick = function() {
   var imageName = window.getComputedStyle(document.body)["background-image"];
   if (imageName.indexOf("red") > -1) {
       document.body.style.setProperty("background-image", "url('ansible_green.png')");
+      window.open('https://jinshuju.net/f/eW4zGI', '_blank');
   } else {
       document.body.style.setProperty("background-image", "url('ansible_red.png')");
   }
+
 }
